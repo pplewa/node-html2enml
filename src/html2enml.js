ae5b3b7cd2071c66763547a8dc43a67c85a736a5
Peter Plewa <pplewa@atlassian.com>
2018-08-10 11:48:19 +1000

    decaffeinate

---
+++ src/html2enml.js
@@ -0,0 +1,399 @@
+const fs = require('fs')
+const async = require('async')
+const mime = require('mime')
+mime.default_type = ''
+const { XMLHttpRequest } = require('xmlhttprequest')
+const SparkMD5 = require('spark-md5')
+let { DOMParser, NodeType, XMLSerializer } = require('xmldom')
+const Evernote = require('evernote')
+
+// Helper to check the head of our URL strings
+if (String.prototype.startsWith == null) {
+	String.prototype.startsWith = function(s) {
+		return this.slice(0, s.length) === s
+	}
+}
+if (String.prototype.startsWithAny == null) {
+	String.prototype.startsWithAny = function(s) {
+		for (let x of Array.from(s)) {
+			if (this.startsWith(x)) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+const NOTE_HEADER = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM \
+"http://xml.evernote.com/pub/enml2.dtd">`
+
+const PERMITTED_ELEMENTS = [
+	'a',
+	'abbr',
+	'acronym',
+	'address',
+	'area',
+	'b',
+	'bdo',
+	'big',
+	'blockquote',
+	'br',
+	'caption',
+	'center',
+	'cite',
+	'code',
+	'col',
+	'colgroup',
+	'dd',
+	'del',
+	'dfn',
+	'div',
+	'dl',
+	'dt',
+	'em',
+	'font',
+	'h1',
+	'h2',
+	'h3',
+	'h4',
+	'h5',
+	'h6',
+	'hr',
+	'i',
+	'img',
+	'ins',
+	'kbd',
+	'li',
+	'map',
+	'ol',
+	'p',
+	'pre',
+	'q',
+	's',
+	'samp',
+	'small',
+	'span',
+	'strike',
+	'strong',
+	'sub',
+	'sup',
+	'table',
+	'tbody',
+	'td',
+	'tfoot',
+	'th',
+	'thead',
+	'title',
+	'tr',
+	'tt',
+	'u',
+	'ul',
+	'var',
+	'xmp'
+]
+
+const PROHIBITED_ATTR = [
+	'id',
+	'class',
+	'onclick',
+	'ondblclick',
+	'on',
+	'accesskey',
+	'data',
+	'dynsrc',
+	'tabindex'
+]
+
+// Node types as given in xmldom
+NodeType = {}
+const ELEMENT_NODE = (NodeType.ELEMENT_NODE = 1)
+const ATTRIBUTE_NODE = (NodeType.ATTRIBUTE_NODE = 2)
+const TEXT_NODE = (NodeType.TEXT_NODE = 3)
+const CDATA_SECTION_NODE = (NodeType.CDATA_SECTION_NODE = 4)
+const ENTITY_REFERENCE_NODE = (NodeType.ENTITY_REFERENCE_NODE = 5)
+const ENTITY_NODE = (NodeType.ENTITY_NODE = 6)
+const PROCESSING_INSTRUCTION_NODE = (NodeType.PROCESSING_INSTRUCTION_NODE = 7)
+const COMMENT_NODE = (NodeType.COMMENT_NODE = 8)
+const DOCUMENT_NODE = (NodeType.DOCUMENT_NODE = 9)
+const DOCUMENT_TYPE_NODE = (NodeType.DOCUMENT_TYPE_NODE = 10)
+const DOCUMENT_FRAGMENT_NODE = (NodeType.DOCUMENT_FRAGMENT_NODE = 11)
+const NOTATION_NODE = (NodeType.NOTATION_NODE = 12)
+
+const PERMITTED_URLS = ['http', 'https', 'file', 'evernote']
+
+class htmlEnmlConverter {
+	constructor(options) {
+		this._convertNode = this._convertNode.bind(this)
+		this.baseUrl = options.baseUrl || ''
+		this.strict = options.strict || false
+		this.includeComments = options.includeComments || false
+		this.ignoreFiles = options.ignoreFiles || false
+		this.resources = []
+		this.warnings = []
+		this.errors = []
+		this.fatalErrors = []
+		this.parser = new DOMParser({
+			errorHandler: {
+				warning: mess => this.warnings.push(mess),
+				error: mess => this.errors.push(mess),
+				fatalError: mess => this.fatalErrors.push(mess)
+			}
+		})
+		this.serializer = new XMLSerializer()
+	}
+
+	convert(htmlString, callback) {
+		const doc = this.parser.parseFromString(htmlString, 'text/html')
+		if (this.fatalErrors.length) {
+			// Catching non-recoverable errors
+			return callback(new Error(this.fatalErrors))
+		}
+		let body = doc.getElementsByTagName('body')
+		if (!body.length) {
+			return callback(new Error('Body element not found'))
+		} else {
+			body = body[0]
+		}
+		return this._convertBody(body, err => {
+			if (err) {
+				return callback(err)
+			}
+			const enml = NOTE_HEADER + this.serializer.serializeToString(body)
+			const resources = this.resources.map(e => e.resource)
+
+			return callback(null, enml, resources)
+		})
+	}
+
+	_convertBody(domNode, callback) {
+		domNode.tagName = 'en-note'
+		return async.series(
+			[
+				callback => {
+					return async.each(
+						domNode.attributes,
+						(attribute, callback) => {
+							const attributeName = attribute.name.toLowerCase()
+							if (Array.from(PROHIBITED_ATTR).includes(attributeName)) {
+								// Discard attribute since not allowed in ENML
+								domNode.attributes.removeNamedItem(attribute.name)
+							}
+							return callback()
+						},
+						callback
+					)
+				},
+				callback => {
+					// Handle element children
+					return async.each(domNode.childNodes, this._convertNode, callback)
+				}
+			],
+			callback
+		)
+	}
+
+	_convertNode(domNode, callback) {
+		// We only need to process element nodes:
+		// unless domNode.nodeType is ELEMENT_NODE
+		//   return callback()
+		switch (domNode.nodeType) {
+			case ELEMENT_NODE:
+				return this._convertElementNode(domNode, callback)
+			case TEXT_NODE:
+				return callback()
+			case COMMENT_NODE:
+				if (!this.includeComments) {
+					domNode.parentNode.removeChild(domNode)
+				}
+				return callback()
+			default:
+				domNode.parentNode.removeChild(domNode)
+				return callback()
+		}
+	}
+
+	_convertElementNode(domNode, callback) {
+		const tagName = domNode.tagName ? domNode.tagName.toLowerCase() : ''
+		if (!Array.from(PERMITTED_ELEMENTS).includes(tagName)) {
+			// Discard element if not permitted in ENML
+			domNode.parentNode.removeChild(domNode)
+			const err = this.strict ? new Error(`Illegal element (${tagName})`) : null
+			return callback(err)
+		}
+
+		return async.parallel(
+			[
+				callback => {
+					// Handle element attributes
+					return async.each(
+						domNode.attributes,
+						(attribute, callback) => {
+							const attributeName = attribute.name.toLowerCase()
+							if (Array.from(PROHIBITED_ATTR).includes(attributeName)) {
+								// Discard attribute since not allowed in ENML
+								domNode.attributes.removeNamedItem(attribute.name)
+								return callback()
+							} else if (attributeName === 'href' && tagName === 'a') {
+								// Convert relative links to absolute links
+								attribute.value = this._adjustUrl(attribute.value)
+								if (!attribute.value) {
+									domNode.attributes.removeNamedItem(attribute.name)
+								}
+								return callback()
+							} else if (attributeName === 'src' && tagName === 'img') {
+								if (this.ignoreFiles) {
+									domNode.parentNode.removeChild(domNode)
+									return callback()
+								}
+								attribute.value = this._adjustUrl(attribute.value)
+								if (!attribute.value) {
+									domNode.parentNode.removeChild(domNode)
+									return callback()
+								} else {
+									// Convert images to Evernote resources
+									return this._convertMedia(domNode, attribute.value, callback)
+								}
+							} else {
+								return callback()
+							}
+						},
+						callback
+					)
+				},
+				callback => {
+					// Handle element children
+					return async.each(domNode.childNodes, this._convertNode, callback)
+				}
+			],
+			callback
+		)
+	}
+
+	_convertMedia(element, url, callback) {
+		// Test if resource already loaded
+		let resource = this.resources.find(resource => resource.url === url)
+
+		// Resource already present
+		if (resource) {
+			element.tagName = 'en-media'
+			element.setAttribute('hash', resource.hash)
+			element.setAttribute('type', resource.mime)
+			element.removeAttribute('src')
+			return callback()
+		}
+
+		// Resource not present: Send new request
+		const xhr = new XMLHttpRequest()
+		xhr.element = element
+		xhr.responseType = 'arraybuffer'
+		const _this = this
+		xhr.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					// Identify mime type
+					const mimeType =
+						this.getResponseHeader('content-type') || mime.lookup(url)
+					if (!mimeType) {
+						// Mime type will be empty if it cannot be identified
+						if (_this.strict) {
+							// Throw error in strict mode
+							return callback(
+								new Error(
+									`Mime type of resource ${url} could not be identified`
+								)
+							)
+						} else {
+							// when not in strict mode: ignore error and remove domNode
+							element.parentNode.removeChild(element)
+							return callback()
+						}
+					}
+
+					// Create file hash
+					const spark = new SparkMD5.ArrayBuffer()
+					spark.append(this.responseText)
+					const hash = spark.end()
+
+					// Create new Evernote resource
+					resource = Evernote.Types.Resource({
+						mime: mimeType
+					})
+					resource.data = Evernote.Types.Data()
+					resource.data.body = _this._toBuffer(this.responseText)
+					resource.data.bodyHash = hash
+
+					// Prepare ENML element
+					this.element.tagName = 'en-media'
+					this.element.setAttribute('hash', hash)
+					this.element.setAttribute('type', mimeType)
+					this.element.removeAttribute('src')
+
+					// Add resource to resource lookup table
+					_this.resources.push({
+						url,
+						hash,
+						mime: mimeType,
+						resource
+					})
+					return callback()
+				} else {
+					// Handle case when resource not found here
+					if (_this.strict) {
+						// Throw error in strict mode
+						return callback(
+							new Error(`Resource ${url} not found (${this.status})`)
+						)
+					} else {
+						// Remove element otherwise
+						element.parentNode.removeChild(element)
+						return callback()
+					}
+				}
+			}
+		}
+		xhr.open('GET', url)
+		return xhr.send()
+	}
+
+	_toBuffer(arrayBuff) {
+		console.log(arrayBuff)
+		const buf = new Buffer(arrayBuff.byteLength)
+		const view = new Uint8Array(arrayBuff)
+		for (var i = 0; i < buf.length; ++i) {
+			buf[i] = view[i]
+		}
+		return buf
+	}
+
+	_adjustUrl(relative) {
+		if (relative.startsWithAny(PERMITTED_URLS)) {
+			return relative
+		}
+		const stack = this.baseUrl.split('/')
+		const parts = relative.split('/')
+		stack.pop()
+		for (let part of Array.from(parts)) {
+			if (part === '.') {
+				continue
+			}
+			if (part === '..') {
+				stack.pop()
+			} else {
+				stack.push(part)
+			}
+		}
+		return stack.join('/')
+	}
+}
+
+module.exports.fromString = (htmlString, options, callback) =>
+	new htmlEnmlConverter(options).convert(htmlString, callback)
+
+module.exports.fromFile = (file, options, callback) =>
+	// Read file to string
+	fs.readFile(file, 'utf8', function(err, htmlString) {
+		if (err) {
+			callback(err)
+		}
+		return new htmlEnmlConverter(options).convert(htmlString, callback)
+	})
---
+++ test/test.js
@@ -0,0 +1,383 @@
+const { expect } = require('chai')
+const html2enml = require('../src/html2enml')
+const path = require('path')
+const fs = require('fs')
+
+describe('html2enml', function() {
+	const options = { baseUrl: 'http://www.google.com/' }
+
+	describe('.fromString()', function() {
+		const resource = {
+			mime: 'image/png',
+			data: {
+				bodyHash: 'd0ad09ba8fe3801ac437d06ba62740d2',
+				_body:
+					'�PNG\r\n\u001a\n\u0000\u0000\u0000\rIHDR\u0000\u0000\u0000 \u0000\u0000\u0000 \u0002\u0003\u0000\u0000\u0000\u000e\u0014�g\u0000\u0000\u0000\u0004gAMA\u0000\u0001��1��_\u0000\u0000\u0000\u0003sBIT\u0001\u0001\u0001|.w�\u0000\u0000\u0000\fPLTE\u0000�\u0000�\u0000\u0000��\u0000\u0000\u0000�e?+�\u0000\u0000\u0000"IDATx�c�\u001f��\u0001�\u0019�0���\u0018��,|\f����\u0018��=\u0000�I�ꉎ\u001b\u0000\u0000\u0000\u0000IEND�B`�'
+			}
+		}
+
+		it('converts basic HTML to ENML', function(done) {
+			const plainHTML = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="http://www.google.com">And a link.</a></p> \
+</body> \
+</html>`
+			return html2enml.fromString(plainHTML, options, function(err, enml) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="http://www.google.com">And a link.</a></p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				return done()
+			})
+		})
+
+		it("converts relative URL's to absolute URL's", function(done) {
+			const relativeHTML = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="resource">And a link.</a></p> \
+</body> \
+</html>`
+			return html2enml.fromString(relativeHTML, options, function(err, enml) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="http://www.google.com/resource">And a link.</a></p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				return done()
+			})
+		})
+
+		it("converts internal Evernote URL's", function(done) {
+			// Pseudo Evernote notebook url
+			const relativeHTML = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="evernote:///view/2127483637/s161/01344ac2-be5a-454b-8e01-a0b487cda7e4/01344ac2-be5a-454b-8e01-a0c987cda7e4/">And a link.</a></p> \
+</body> \
+</html>`
+			return html2enml.fromString(relativeHTML, options, function(err, enml) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> <p>Some text. <a href="evernote:///view/2127483637/s161/01344ac2-be5a-454b-8e01-a0b487cda7e4/01344ac2-be5a-454b-8e01-a0c987cda7e4/">And a link.</a></p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				return done()
+			})
+		})
+
+		it('converts files to ENML resouces', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg.png'
+			)}\"></p> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, options, function(
+				err,
+				enml,
+				resources
+			) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. <en-media hash="d0ad09ba8fe3801ac437d06ba62740d2" type="image/png"></en-media></p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				expect(resources.length).to.equal(1)
+				expect(resources[0].data.bodyHash).to.equal(resource.data.bodyHash)
+				expect(resources[0].data._body).to.equal(resource.data.body)
+				expect(resources[0].mime).to.equal(resource.mime)
+				return done()
+			})
+		})
+
+		it('discards files if ignoreFiles flag is set', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg.png'
+			)}\"></p> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, { ignoreFiles: true }, function(
+				err,
+				enml,
+				resources
+			) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. </p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				expect(resources.length).to.equal(0)
+				return done()
+			})
+		})
+
+		it('discards comments by default', function(done) {
+			// Pseudo Evernote notebook url
+			const commentedHTML = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="evernote:///view/2127483637/s161/01344ac2-be5a-454b-8e01-a0b487cda7e4/01344ac2-be5a-454b-8e01-a0c987cda7e4/">And a link.</a></p><!--This is a comment.--> \
+</body> \
+</html>`
+			return html2enml.fromString(commentedHTML, options, function(err, enml) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="evernote:///view/2127483637/s161/01344ac2-be5a-454b-8e01-a0b487cda7e4/01344ac2-be5a-454b-8e01-a0c987cda7e4/">And a link.</a></p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				return done()
+			})
+		})
+
+		it('includes comments if includeComments flag set', function(done) {
+			// Pseudo Evernote notebook url
+			const commentedHTML = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="evernote:///view/2127483637/s161/01344ac2-be5a-454b-8e01-a0b487cda7e4/01344ac2-be5a-454b-8e01-a0c987cda7e4/">And a link.</a></p> \
+<!--This is a comment.--> \
+</body> \
+</html>`
+			return html2enml.fromString(
+				commentedHTML,
+				{ includeComments: true },
+				function(err, enml) {
+					const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. <a href="evernote:///view/2127483637/s161/01344ac2-be5a-454b-8e01-a0b487cda7e4/01344ac2-be5a-454b-8e01-a0c987cda7e4/">And a link.</a></p> \
+<!--This is a comment.--> \
+</en-note>`
+					expect(err).to.be.null
+					expect(enml).to.equal(enmlExpected)
+					return done()
+				}
+			)
+		})
+
+		it('discards files if resources not found and not in strict mode', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg'
+			)}\"></p> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, options, function(
+				err,
+				enml,
+				resources
+			) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. </p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				expect(resources.length).to.equal(0)
+				return done()
+			})
+		})
+
+		it('throws error if resources not found and in strict mode', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg'
+			)}\"></p> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, { strict: true }, function(
+				err,
+				enml,
+				resources
+			) {
+				expect(err).to.not.be.null
+				expect(enml).to.be.undefined
+				expect(resources).to.be.undefined
+				return done()
+			})
+		})
+
+		it('discards files if resource invalid and not in strict mode', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg.unknown'
+			)}\"></p> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, options, function(
+				err,
+				enml,
+				resources
+			) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1> \
+<p>Some text. </p> \
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				expect(resources.length).to.equal(0)
+				return done()
+			})
+		})
+
+		it('throws error if resource invalid and in strict mode', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg.unknown'
+			)}\"></p> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, { strict: true }, function(
+				err,
+				enml,
+				resources
+			) {
+				expect(err).to.not.be.null
+				expect(enml).to.be.undefined
+				expect(resources).to.be.undefined
+				return done()
+			})
+		})
+
+		it('discards element if encountering invalid tag when not in strict mode', function(done) {
+			// form element not permitted in ENML
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<form>Some text.</form> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, options, function(
+				err,
+				enml,
+				resources
+			) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml"> \
+<h1>A Heading</h1>  </en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				expect(resources.length).to.equal(0)
+				return done()
+			})
+		})
+
+		it('throws error if encountering invalid tag in strict mode', function(done) {
+			// form element not permitted in ENML
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<body> \
+<h1>A Heading</h1> \
+<form>Some text.</form> \
+</body> \
+</html>`
+			return html2enml.fromString(fileHtml, { strict: true }, function(
+				err,
+				enml,
+				resources
+			) {
+				expect(err).to.not.be.null
+				expect(enml).to.be.undefined
+				expect(resources).to.be.undefined
+				return done()
+			})
+		})
+
+		it('throws error if HTML lacks body element', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<h1>A Heading</h1> \
+<p>Some text. <img src=\"file:${path.join(
+				__dirname,
+				'assets',
+				'testImg'
+			)}\"></p> \
+</html>`
+			return html2enml.fromString(fileHtml, { strict: true }, function(
+				err,
+				enml,
+				resources
+			) {
+				expect(err).to.not.be.null
+				expect(enml).to.be.undefined
+				expect(resources).to.be.undefined
+				return done()
+			})
+		})
+
+		return it('throws error if HTML cannot be parsed', function(done) {
+			const fileHtml = `<!DOCTYPE html> \
+<html> \
+<h1 att=3D\"att\">A Heading</h1> \
+<p>Some text.</p> \
+</html>`
+			return html2enml.fromString(fileHtml, { strict: true }, function(
+				err,
+				enml,
+				resources
+			) {
+				expect(err).to.not.be.null
+				expect(enml).to.be.undefined
+				expect(resources).to.be.undefined
+				return done()
+			})
+		})
+	})
+
+	return describe('.fromFile()', () =>
+		it('converts file at given path', function(done) {
+			const filepath = path.join(__dirname, 'assets', 'testHtml.html')
+			return html2enml.fromFile(filepath, options, function(err, enml) {
+				const enmlExpected = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note xmlns="http://www.w3.org/1999/xhtml">
+<h1>A Heading</h1>
+<p>Some text. <a href="http://www.google.com">And a link.</a></p>
+</en-note>`
+				expect(err).to.be.null
+				expect(enml).to.equal(enmlExpected)
+				return done()
+			})
+		}))
+})
